ae303eae09ad6e0f267cb20fe525c80fb9af7136
Jordan <jordanhernando1085@gmail.com>
2021-01-24 16:25:13 -0300

    Instanciar flask y configurar mongodb

--- project_app/__init__.py
+++ project_app/__init__.py
@@ -1,1 +1,20 @@
 #modulo en python
+
+#Configuracion inicial http://docs.mongoengine.org/projects/flask-mongoengine/en/latest/
+from flask import Flask
+from flask_mongoengine import MongoEngine
+
+"""
+MongoEngine = es un conector de mongo al igual que pymoongo
+"""
+
+#instanciando flask
+app = Flask(__name__)
+
+#configuraciones de la base de datos 
+app.config['MONGODB_SETTINGS'] = {
+    'db' : 'flaskBootstrap'
+}
+
+#cargando la configuracion con MongoEngine
+db = MongoEngine(app)
